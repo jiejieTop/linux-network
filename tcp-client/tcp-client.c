06a6d118f6ffbaa54ace186604f479f77a35ae79
jiejie <1161959934@qq.com>
2019-11-18 13:01:51 +0000

    add tcp client demo jiejie 2019-11-18

--- tcp-client/tcp-client.c
+++ tcp-client/tcp-client.c
@@ -1,3 +1,10 @@
+/*
+ * @Author: jiejie
+ * @Github: https://github.com/jiejieTop
+ * @Date: 2019-11-14 00:54:15
+ * @LastEditTime: 2019-11-18 12:59:10
+ * @Description: the code belongs to jiejie, please keep the author information and source code according to the license.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,50 +15,64 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define DOMAIN_NAME "www.jiejietop.cn"
-#define PORT 80
-#define MAX_DATA (14*1024)
+//#define HOST "jiedev.com"
+#define HOST "192.168.0.3"
+#define PORT 5002
+#define MAX_DATA (10*1024)
+
 
 int main(void)
 {
-    int sockfd, num;
-    struct hostent *hptr;
+    int sockfd;
+    struct hostent *he;
+    struct in_addr addr;
     struct sockaddr_in server;
     char recv_data[MAX_DATA];
 
-	if ((hptr = gethostbyname(DOMAIN_NAME)) == NULL) {
-		printf("get host ip addr error.\n");
-		exit(1);
-	} else {
-        printf("host name:%s, ip:%s\n",DOMAIN_NAME, inet_ntoa(*((struct in_addr *)hptr->h_addr)));
+    if (inet_aton(HOST, &addr) == 0) {
+        printf("get %s ip addr...\n", HOST);
+        if ((he = gethostbyname(HOST)) == NULL) {
+            printf("get host ip addr error.\n");
+            exit(EXIT_FAILURE);
+        } else {
+            addr = *((struct in_addr *)he->h_addr); 
+            printf("host name: %s, ip addr:%s\n",HOST, inet_ntoa(addr));
+        }
+    } else {
+        printf("host addr:%s\n",inet_ntoa(addr));
     }
 
-    if (socket(AF_INET, SOCK_STREAM, 0) == -1) {
+    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
         printf("create an endpoint for communication fail!\n");
         exit(1);
     } 
-    
 
-    bzero(&server, sizeof(server));
+    bzero(&server, sizeof(struct sockaddr));
 	server.sin_family = AF_INET;
 	server.sin_port = htons(PORT);
-	server.sin_addr = *((struct in_addr *)hptr->h_addr);
+	server.sin_addr = addr;
 
-    if (connect(sockfd, (struct sockaddr *)&server, sizeof(server)) == -1) {
+    if (connect(sockfd, (struct sockaddr *)&server, sizeof(struct sockaddr)) == -1) {
         printf("connect server fail\n");
+        close(sockfd);
         exit(1);
-    } else {
-        printf("connect server success\n");
-        
-        if ((num = recv(sockfd, recv_data, MAX_DATA, 0) == -1)) {
+    } 
+
+    printf("connect server success\n");
+
+    while (1) {
+
+        if (recv(sockfd, recv_data, MAX_DATA, 0)) {
+            printf("server message: %s.\n", recv_data);
+            memset(recv_data,0,MAX_DATA); 
+        } else {
             printf("recv data fail!\n");
+            close(sockfd);
             exit(1);
         }
-
-        recv_data[num] = '\0';
-	    printf("server message: %s.\n", recv_data);
-	    close(sockfd);
     }
 
+    close(sockfd);
     exit(0);
 }
+
